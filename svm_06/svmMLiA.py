2736bb91406a3a912b6c67fd017063bd850c784c
j616720816 <15111446587@163,com>
2019-11-13 16:51:13 +0800

    66_4停止了，补基础

--- svm_06/svmMLiA.py
+++ svm_06/svmMLiA.py
@@ -34,11 +34,13 @@ def clipAlpha(aj, H, L):
 '''6_2简化版SMO算法
 2019_11_10
 理解欠缺，还需回头继续看
+2019_11_11
+算法原型在《统计学习方法(第二版)》p143
 '''
 
 #dataMatIn: 数据集 100*2
 #classLabels: 类别标签 100*1
-#C: 常数C
+#C: 常数C，惩罚系数
 #toler: 容错率
 #maxIter: 退出当前最大的循环次数
 def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
@@ -56,7 +58,8 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
         for i in range(m):
             #计算出预测的类别
             #multiply:数组和矩阵对应位置相乘，输出与相乘数组/矩阵大小一致
-            #*:对数组执行对应位置相乘，对矩阵执行矩阵乘法运算   
+            #*:对数组执行对应位置相乘，对矩阵执行矩阵乘法运算 
+            # p145 7.104  
             fXi = float(np.multiply(alphas, labelMat).T*\
                 (dataMatrix * dataMatrix[i, :].T)) + b
             #计算与标签之间的误差
@@ -67,12 +70,19 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
                 (alphas[i] > 0)):
                 #2 随机选择第二个alpha
                 j = selectJrand(i, m)
+                #p145 公式7.104
                 fXj = float(np.multiply(alphas, labelMat).T*\
                     (dataMatrix * dataMatrix[j, :].T)) + b
+                #p145 公式7.105
                 Ej = fXj - float(labelMat[j])
+                #copy():https://www.runoob.com/python/att-dictionary-copy.html
+                #dict2 = dict1          # 浅拷贝: 引用对象
+                #dict3 = dict1.copy()   # 浅拷贝：深拷贝父对象（一级目录），子对象（二级目录）不拷贝，还是引用
                 alphaIold = alphas[i].copy()
                 alphaJold = alphas[j].copy()
                 #3 保证alpha在0和C之间
+                #p144最后一行，p145第一行 
+                #分别是y1!=y2和y1=y2两种情况
                 if (labelMat[i] != labelMat[j]):
                     L = max(0, alphas[j] - alphas[i])
                     H = min(C, C + alphas[j] - alphas[i])
@@ -83,12 +93,14 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
                     print("L == H")
                     continue
                 #eta是alpha[j]的最优修改量
+                #p145 7.107
                 eta = 2.0 * dataMatrix[i, :] * dataMatrix[j, :].T - \
                     dataMatrix[i, :] * dataMatrix[i, :].T - \
                     dataMatrix[j, :] * dataMatrix[j, :].T
                 if eta >= 0:
                     print("eta >= 0")
                     continue
+                #p145 7.106
                 alphas[j] -= labelMat[j] * (Ei - Ej) / eta
                 alphas[j] = clipAlpha(alphas[j], H, L)
                 if (abs(alphas[j] - alphaJold) < 0.00001):
@@ -98,10 +110,12 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
                 alphas[i] += labelMat[j] * labelMat[i] * \
                     (alphaJold - alphas[j])
                 #5 设置常数项
+                #p148 7.115
                 b1 = b - Ei - labelMat[i] * (alphas[i] - alphaIold) * \
                     dataMatrix[i, :] * dataMatrix[i, :].T - \
                     labelMat[j] * (alphas[j] - alphaJold) * \
                     dataMatrix[i, :] * dataMatrix[j, :].T
+                #p148 7.116
                 b2 = b - Ej -labelMat[i] * (alphas[i] - alphaIold) * \
                      dataMatrix[i, :] * dataMatrix[j, :].T - \
                      labelMat[j] * (alphas[j] - alphaJold) * \
@@ -122,6 +136,86 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
         print("iteration number: %d" % iter)
     return b, alphas
             
+'''6_3完整版Platt SMO的支持函数
+2019_11_11
+'''
+
+#建立一个数据结构来保存所有的重要值，使用对象
+#数据可以作为一个对象进行传递
+class optStruct:
+    def __init__(self, dataMatIn, classLabels, C, toler):
+        self.X = dataMatIn
+        self.labelMat = classLabels
+        self.C = C
+        self.tol = toler
+        self.m = np.shape(dataMatIn)[0]
+        self.alphas = np.mat(np.zeros((self.m, 1)))
+        self.b = 0
+        #1 误差缓存
+        self.eCache = np.mat(np.zeros((self.m, 2)))
+
+#能够计算E值，并且返回
+def calcEk(oS, k):
+    fXk = float(np.multiply(oS.alphas, oS.labelMat).T *\
+        (oS.X * oS.X[k, :].T)) + oS.b
+    Ek = fXk -float(oS.labelMat[k])
+    return Ek
+
+#2内循环的启发式方法，用于第二个alpha
+def selectJ(i, oS, Ei):
+    maxK = -1
+    maxDeltaE = 0
+    Ej = 0
+    oS.eCache[i] = [1, Ei]
+    validEcacheList = np.nonzero(oS.eCache[:, 0].A)[0]
+    if (len(validEcacheList)) > 1:
+        for k in validEcacheList:
+            if k == i:
+                continue
+            Ek = calcEk(oS, k)
+            deltaE = abs(Ei - Ek)
+            #3选择具有最大步长的j
+            if (deltaE > maxDeltaE):
+                maxK = k
+                maxDeltaE = deltaE
+                Ej = Ek
+        return maxK, Ej
+    else:
+        j = selectJrand(i, oS.m)
+        Ej = calcEk(oS, j)
+    return j, Ej
+
+#计算误差值并存入缓存，对alpha值进行优化之后使用
+def updataEk(oS, k):
+    Ek = calcEk(oS, k)
+    oS.eCache[k] = [1, Ek]
+
+'''6_4完整Platt SMO算法中的优化例程
+2019_11_13
+'''
+
+def innerL(i, oS):
+    Ei = calcEk(oS, i)
+    if ((oS.labelMat[i] * Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or\
+        ((oS.labelMat[i] * Ei > oS.tol) and (oS.alphas[i] > 0)):
+        j, Ej = selectJ(i, oS, Ei)
+        alphaIold = oS.alphas[i].copy()
+        alphaJold = oS.alphas[j].copy()
+        if (oS.labelMat[i] != oS.labelMat[j]):
+            L = max(0, oS.alphas[j] - oS.alphas[i])
+            H = min(oS.C, oS.C + oS.alphas[j] - oS.alphas[i])
+        else:
+            L = max(0, oS.alphas[j] + oS.alphas[i] - oS.C)
+            H = min(oS.C, oS.alpha)
+
+            
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     #6_1SMO算法中的辅助函数测试
